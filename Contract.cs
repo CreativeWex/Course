9b41cded17facd4b6ebc0945344f6d787f529f41
Wex <bernikcooldude@yandex.ru>
2021-11-19 19:45:55 +0300

    Update

--- Contract.cs
+++ Contract.cs
@@ -7,57 +7,57 @@ using System.IO;
 
 namespace Course
 {
-	class Contract
-	{
-		int numberOfContract; //Номер договора
-		string date; // Дата заключения
-		double summ; // Страховая сумма
-		double tariffRate; // Тарифная ставка
-		string branch; // Филиал
-		string insuranceType; //Вид страхования
+	//class Contract
+	//{
+	//	int numberOfContract; //Номер договора
+	//	string date; // Дата заключения
+	//	double summ; // Страховая сумма
+	//	double tariffRate; // Тарифная ставка
+	//	string branch; // Филиал
+	//	string insuranceType; //Вид страхования
 
-		public Contract(int numberOfContract, string date, double summ, double tariffRate, string branch, string insuranceType)
-		{
-			this.numberOfContract = numberOfContract;
-			this.date = date;
-			this.summ = summ;
-			this.tariffRate = tariffRate;
-			this.branch = branch;
-			this.insuranceType = insuranceType;
-		}
-		public int NUMBER_OF_CONTRACT
-		{
-			get { return numberOfContract; }
-		}
+	//	public Contract(int numberOfContract, string date, double summ, double tariffRate, string branch, string insuranceType)
+	//	{
+	//		this.numberOfContract = numberOfContract;
+	//		this.date = date;
+	//		this.summ = summ;
+	//		this.tariffRate = tariffRate;
+	//		this.branch = branch;
+	//		this.insuranceType = insuranceType;
+	//	}
+	//	public int NUMBER_OF_CONTRACT
+	//	{
+	//		get { return numberOfContract; }
+	//	}
 
-		public string DATE
-		{
-			get { return date; }
-		}
+	//	public string DATE
+	//	{
+	//		get { return date; }
+	//	}
 
-		public double SUMM
-		{
-			get { return summ; }
-		}
+	//	public double SUMM
+	//	{
+	//		get { return summ; }
+	//	}
 
-		public double TARIFF_RATE
-		{
-			get { return tariffRate; }
-		}
+	//	public double TARIFF_RATE
+	//	{
+	//		get { return tariffRate; }
+	//	}
 
-		public string BRANCH
-		{
-			get { return branch; }
-		}
+	//	public string BRANCH
+	//	{
+	//		get { return branch; }
+	//	}
 
-		public string INSURANCE_TYPE
-		{
-			get { return insuranceType; }
-		}
+	//	public string INSURANCE_TYPE
+	//	{
+	//		get { return insuranceType; }
+	//	}
 
-		public void Display()
-		{
-			Console.WriteLine($"|Номер договора: {numberOfContract}, дата заключения договора: {date}, страховая сумма: {summ}, тарифная ставка {tariffRate}, филиал {branch}, вид страхования {insuranceType} |");
-		}
-	}
+	//	public void Display()
+	//	{
+	//		Console.WriteLine($"|Номер договора: {numberOfContract}, дата заключения договора: {date}, страховая сумма: {summ}, тарифная ставка {tariffRate}, филиал {branch}, вид страхования {insuranceType} |");
+	//	}
+	//}
 }
