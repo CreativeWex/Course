8b4bc14632af6faea80fa8c01b0faa591f358f25
Wex <bernikcooldude@yandex.ru>
2021-11-19 18:43:47 +0300

    Added Roadmap

--- Class.cs
+++ Class.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Course
 {
-    class Ancestor
+    class Ancestor // Для наследования всех классов кроме Contracts
     {
+        string name; //Наименование
+
+        public Ancestor(string name)
+        {
+            this.name = name;
+        }
     }
+
+    class Branch:Ancestor // Класс филиал
+    {
+        int code; // Код филиала
+        // Наименование филиала наследуется
+        string adress;
+        string phoneNumber;
+
+        public Branch(string name, int code, string adress, string phoneNumber) : base (name)
+        {
+            this.code = code;
+            this.adress = adress;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    class Branch : Ancestor // Класс филиал
+    {
+        int code; // Код филиала
+        // Наименование филиала наследуется
+        string adress;
+        string phoneNumber;
+
+        public Branch(string name, int code, string adress, string phoneNumber) : base(name)
+        {
+            this.code = code;
+            this.adress = adress;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
 }
